1ab93f198c822e76640550107f25c86ce38c4106
Nicholas <nick.nogue28@outlook.com>
2024-10-24 20:19:43 -0300

    add db

--- golang/cmd/videoconverter/main.go
+++ golang/cmd/videoconverter/main.go
@@ -1,8 +1,53 @@
 package main
 
-import "github.com/nichol20/fctube/internal/converter"
+import (
+	"database/sql"
+	"fmt"
+	"log/slog"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/nichol20/fctube/internal/converter"
+)
+
+// connectPostgres establishes a connection with PostgreSQL using environment variables for configuration.
+func connectPostgres() (*sql.DB, error) {
+	user := getEnvOrDefault("POSTGRES_USER", "user")
+	password := getEnvOrDefault("POSTGRES_PASSWORD", "password")
+	dbname := getEnvOrDefault("POSTGRES_DB", "converter")
+	host := getEnvOrDefault("POSTGRES_HOST", "host.docker.internal")
+	sslmode := getEnvOrDefault("POSTGRES_SSL_MODE", "disable")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s", user, password, dbname, host, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		slog.Error("Failed to connect to PostgreSQL", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		slog.Error("Failed to ping PostgreSQL", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	slog.Info("Connected to PostgreSQL successfully")
+	return db, nil
+}
+
+// getEnvOrDefault fetches the value of an environment variable or returns a default value if it's not set.
+func getEnvOrDefault(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
 
 func main() {
-	vc := converter.NewVideoConverter()
-	vc.Handle([]byte(`{"video_id": 1, "path": "/media/uploads/1"}`))
+	db, err := connectPostgres()
+	if err != nil {
+		panic(err)
+	}
+	vc := converter.NewVideoConverter(db)
+	vc.Handle([]byte(`{"video_id": 2, "path": "/media/uploads/2"}`))
 }
